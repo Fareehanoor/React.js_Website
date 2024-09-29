d66c5519faa3ea980a4f6a5184cc05e9d93ca460
Fareeha Noor <fareehaabbas21@gmail.com>
2024-09-28 18:02:05 -0700

    Updated Header,Footer,App Component

--- 03routing/src/App.jsx
+++ 03routing/src/App.jsx
@@ -1,12 +1,38 @@
+import { createRoutesFromElements, RouterProvider } from "react-router-dom";
+import { createBrowserRouter  , Route} from "react-router-dom";
+import RootLayout from "./Components/RootLayout";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import User from "./Components/User";
+import Github from "./Components/Github";
 
+// const router = createBrowserRouter([
+//   { path: "/", element: <RootLayout /> , children: [
+//     { path: "" , element : <Home /> },
+//     { path: "about" , element : <About />},
+//     { path: "contact" , element : <Contact />}
+//   ] },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+   <Route path='/' element={<RootLayout />}>
+     <Route path= '' element={<Home />} />
+     <Route path= 'about' element={<About />} />
+     <Route path= 'contact' element={<Contact />} />
+     <Route path= "user/:userId" element={<User />} />
+     <Route path = "github" element={<Github />} />
+   </Route>
+  )
+);
 
 function App() {
-
   return (
     <>
-
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
